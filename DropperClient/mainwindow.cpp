ec158888de4eaf5a652c4ef9f6e49e0c1445baf4
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-06-30 12:57:24 +0300

    Created not working sample(

--- DropperClient/mainwindow.cpp
+++ DropperClient/mainwindow.cpp
@@ -7,10 +7,13 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
+    socket = new QTcpSocket(this);
+    connect(socket, &QTcpSocket::readyRead, this, &MainWindow::slotReadyRead);
+    connect(socket, &QTcpSocket::disconnected, socket, &QTcpSocket::deleteLater);
+
     ui->sb_port->setRange(1000, 65535);
     ui->le_ip->setInputMask("000.000.000.000;0");
     ui->statusbar->setStyleSheet("color: red;");
-    ui->b_saveInfo->setToolTip("<p><b>Note:</b> Checkboxes save the user's input in the fields.</p>");
 
     connect(ui->cb_ip,    &QCheckBox::stateChanged, this, &MainWindow::updateInfoButton);
     connect(ui->cb_port,  &QCheckBox::stateChanged, this, &MainWindow::updateInfoButton);
@@ -24,6 +27,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::SendToServer(QString str)
+{
+    Data.clear();
+    QDataStream out(&Data, QIODevice::WriteOnly);
+    out.setVersion(QDataStream::Qt_6_7);
+    out << str;
+    socket->write(Data);
+}
+
 void MainWindow::login()
 {
     if (ui->le_login->text().isEmpty()) {
@@ -31,14 +43,44 @@ void MainWindow::login()
         return;
     }
 
-    QString ip = ui->le_ip->text();
+    QString ip = ui->le_ip->text().trimmed();
+    QStringList parts = ip.split('.');
+    for (QString &part : parts) {
+        int number = part.toInt();
+        part = QString::number(number);
+    }
+    ip = parts.join('.');
     QHostAddress addr;
     if (!addr.setAddress(ip)) {
-        ui->statusbar->showMessage("Invalid IP address format!");
+        ui->statusbar->showMessage("Invalid IP address format: " + ip);
         return;
     }
 
+    socket->connectToHost(ip, ui->sb_port->value());
 
+    connect(socket, &QTcpSocket::errorOccurred, this, [this](QAbstractSocket::SocketError socketError){
+        Q_UNUSED(socketError);
+        ui->statusbar->showMessage(socket->errorString());
+    });
+
+    connect(socket, &QTcpSocket::connected, this, [this](){
+        qDebug() << "Connected to server!";
+        SendToServer(ui->le_login->text());
+        ui->stackedWidget->setCurrentIndex(1);
+    });
+}
+
+void MainWindow::slotReadyRead()
+{
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_6_7);
+    if(in.status() == QDataStream::Ok){
+        QString str;
+        in >> str;
+        ui->te_sendedFiles->append(str);
+    } else {
+        ui->te_sendedFiles->append("read error");
+    }
 }
 
 void MainWindow::updateInfoButton()
@@ -47,4 +89,7 @@ void MainWindow::updateInfoButton()
                     ui->cb_port->isChecked() ||
                     ui->cb_login->isChecked());
     ui->b_saveInfo->setEnabled(enabled);
+
+    if (enabled) ui->b_saveInfo->setToolTip("<p><b>Note:</b> Checkboxes save the user's input in the fields.</p>");
+    else ui->b_saveInfo->setToolTip("");
 }
